049fecb7ce480d2d1c5ee27e3721b8da1f993937
Yamini <yaminiyam0326@gmail.com>
2024-08-19 21:03:49 -0500

    second commint

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Article from "./Article";
+import { useState } from "react";
 
 function Body(){
-    let persons=[ {
+    const [birthdayVal,setBirthdayVal]=useState(5);
+    const [persons,setPersons]=useState([ {
         id: 1,
         name: 'Bertie Yates',
         age: 29,
@@ -36,19 +38,28 @@ function Body(){
         age: 29,
         image:
           'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-      },];
+      },]);
+
+      const clearAll=() =>{
+        setPersons([]);
+        setBirthdayVal(0);
+        val1=0;
+      }
 
     return(
+      
         <>
         <div className="h-screen flex items-center justify-center">
             <div className="bg-white w-[600px] rounded-md font-mono">
-                <h1 className="py-2 px-12 text-3xl">5 Birthdays Today</h1>
-                <div className="py-2"><Article person={persons[0]}/></div>
-                <div className="py-2"><Article className="py-5" person={persons[1]}/></div>
-                <div className="py-2"><Article className="py-5" person={persons[2]}/></div>
-                <div className="py-2"><Article className="py-5" person={persons[3]}/></div>
-                <div className="py-2"><Article className="py-5 " person={persons[4]}/></div>
-                <div className="py-4 px-10"><button className=" w-[500px] bg-cyan-400 rounded-md">Clear All</button></div>
+                <h1 className="py-2 px-12 text-3xl">{birthdayVal} Birthdays Today</h1>
+                {
+                  persons.map((person)=>(
+                    <div className="py-2" key={person.id}>
+                      <Article person={person}/>
+                    </div>
+                  ))
+                }
+                <div className="py-4 px-10"><button className=" w-[500px] bg-cyan-400 rounded-md" onClick={clearAll}>Clear All</button></div>
             </div>
         </div>
         </>
